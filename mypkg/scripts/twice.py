038240da77e51d1e7953daddaa7b443d7e57c368
shouhou-kou <username@example.com>
2021-01-08 17:16:54 +0900

    updt

---
+++ mypkg/scripts/twice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+import pigpio
+from std_msgs.msg import String
+SERVO_PIN =25
+pi=pigpio.pi()
+
+
+def cb(message):
+    rospy.loginfo(message.data)
+    p_width =int(message.data)
+    pi.set_servo_pulsewidth(SERVO_PIN,p_width)
+
+if __name__ == '__main__': 
+    rospy.init_node('twice')
+    sub = rospy.Subscriber('count_up', String, cb) 
+    rospy.spin()
+        
+
